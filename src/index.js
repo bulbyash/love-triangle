817f7054884f7c60cbfdb492fb4d222ec6fda5c0
bulbyash <bulbyash@gmail.com>
2019-03-23 11:10:17 +0300

    feat: getLoveTrianglesCount function

--- src/index.js
+++ src/index.js
@@ -3,5 +3,18 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+   var count = 0;
+   for (var i = 0; i < preferences.length; i++) {
+      var k = preferences[i] - 1;
+      if (k == i) continue;
+      var n = preferences[k] - 1;
+      if (k == n) continue;
+      var m = preferences[n] - 1;
+      if (m == n) continue;
+      if (m == i) {
+          count++;
+          preferences[i] = null;
+      }
+   }
+  return count;
 };
